8d8cb56a0ab0ccb67f6a4ea72af7db03b1384893
Anis Islam <manisul1997bd@gmail.com>
2021-06-22 19:40:24 +0600

    about section created

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import About from '../components/Home/About/About';
 import Header from '../components/Home/Header/Header';
 
 const Home = () => {
     return (
         <main>
             <Header></Header>
+            <About></About>
         </main>
     );
 };
